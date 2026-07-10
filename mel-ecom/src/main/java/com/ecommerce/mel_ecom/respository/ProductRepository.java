453336d1b6cb4a0722a1106e555b13ec987fc08b
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-10 17:37:24 -0400

    Implemented get product by category

--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/respository/ProductRepository.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/respository/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.ecommerce.mel_ecom.respository;
 
 
+import com.ecommerce.mel_ecom.model.Category;
 import com.ecommerce.mel_ecom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
