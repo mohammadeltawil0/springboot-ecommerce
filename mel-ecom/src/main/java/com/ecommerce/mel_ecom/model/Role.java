597942c4065e49e6340b24f3125290cb51cfd383
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-11 17:59:58 -0400

    Implemented user-related model classes

---
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/model/Role.java
@@ -0,0 +1,26 @@
+package com.ecommerce.mel_ecom.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
