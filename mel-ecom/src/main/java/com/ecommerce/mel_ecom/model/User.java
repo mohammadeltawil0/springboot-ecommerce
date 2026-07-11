fed1f281ca1551c63bd1c5e621ad9980bcfe0074
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-11 18:29:49 -0400

    Implemented address class and relationship with users

--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/model/User.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/model/User.java
@@ -6,7 +6,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -59,6 +61,12 @@ public class User {
                 inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses = new ArrayList<>();
 
     @OneToMany(mappedBy = "user",
             cascade = {CascadeType.PERSIST, CascadeType.MERGE},
