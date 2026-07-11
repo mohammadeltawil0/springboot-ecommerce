25116bc51df97ac578107dfc816d638d91b763a9
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-11 15:43:28 -0400

    Implemented pagination and sorting for products

--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/respository/ProductRepository.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/respository/ProductRepository.java
@@ -3,15 +3,14 @@ package com.ecommerce.mel_ecom.respository;
 
 import com.ecommerce.mel_ecom.model.Category;
 import com.ecommerce.mel_ecom.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryOrderByPriceAsc(Category category);
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
 
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
 }
--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/service/ProductService.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/service/ProductService.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 public interface ProductService {
     ProductDTO addProduct(ProductDTO product, Long categoryId);
 
-    ProductResponse getAllProducts();
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-    ProductResponse searchByCategory(Long categoryId);
+    ProductResponse searchByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-    ProductResponse searchProductByKeyword(String keyword);
+    ProductResponse searchProductByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
     ProductDTO updateProduct(Long productId, @Valid ProductDTO product);
 
