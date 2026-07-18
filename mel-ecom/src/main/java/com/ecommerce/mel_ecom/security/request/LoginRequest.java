29100ec04977bebf6720faa76f07e39f02abe081
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-18 17:01:26 -0400

    Added signin endpoint

--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/security/request/LoginRequest.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/security/request/LoginRequest.java
@@ -1,9 +1,14 @@
-package com.ecommerce.mel_ecom.security.jwt;
+package com.ecommerce.mel_ecom.security.request;
 
+
+import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
 
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 
     public String getPassword() {
