c254f54171706d462511a02fc8da7cc6154f81ff
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-08-15 21:59:45 -0400

    Added Swagger annotations in Category Controller

--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/controller/CategoryController.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/controller/CategoryController.java
@@ -5,6 +5,12 @@ import com.ecommerce.mel_ecom.model.Category;
 import com.ecommerce.mel_ecom.payload.CategoryDTO;
 import com.ecommerce.mel_ecom.payload.CategoryResponse;
 import com.ecommerce.mel_ecom.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,6 +35,7 @@ public class CategoryController {
 //        this.categoryService = categoryService;
 //    }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(@RequestParam(name="pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
                                                              @RequestParam(name="pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
@@ -39,20 +46,27 @@ public class CategoryController {
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Create Category", description = "API to create a new category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Category is created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping("/admin/categories")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
+    public ResponseEntity<CategoryDTO> deleteCategory(@Parameter(description = "Category that you wish to delete") @PathVariable Long categoryId) {
         CategoryDTO deletedCategory = categoryService.deleteCategory(categoryId);
         return ResponseEntity.ok(deletedCategory);
     }
 
-
-
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
     @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO, @PathVariable Long categoryId) {
         CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
