c7705cef7cf19e10422a795b1c68afe1e30af266
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-08-15 22:08:14 -0400

    Added Swagger documentation

--- mel-ecom/src/main/java/com/ecommerce/mel_ecom/config/SwaggerConfig.java
+++ mel-ecom/src/main/java/com/ecommerce/mel_ecom/config/SwaggerConfig.java
@@ -1,6 +1,10 @@
 package com.ecommerce.mel_ecom.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -22,6 +26,17 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot E-Commerce REST API")
+                        .version("1.0")
+                        .description("This is an E-Commerce Project that uses Java, Spring Boot, PostgreSQL, AWS, Spring Security, Swagger, Postman, and React")
+                        .license(new License().name("Apache 2.0").url("http://melecom.com"))
+                        .contact(new Contact().name("Mohammad El-Tawil")
+                                .email("mohammadeltawil0@gmail.com")
+                                .url("https://github.com/mohammadeltawil0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Docs")
+                        .url("http://melecom.com"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication", bearerScheme))
                 .addSecurityItem(bearerRequirement);
